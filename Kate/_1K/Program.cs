0aaeda23c9c6d173120d3f443df400e8339cf1b4
Kate <rita15062425@icloud.com>
2024-08-21 22:21:27 +0100

    _1K

---
+++ Kate/_1K/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Reflection.Metadata;
+class Program
+{
+    static void Main()
+    {
+
+        /*
+        #region Pifagor
+        int a = 5;
+        int b = 7;
+        double c = Math.Sqrt(a*a + b*b);
+        c = Math.Round(c, 2);
+        //Console.WriteLine("{0:F2}", c);
+        //Console.WriteLine(c);
+        #endregion
+
+        #region Math.Round
+        double i = 1.675; 
+        double round = Math.Round(i, 2); //1.68
+        double ceiling = Math.Ceiling(i); //2
+        double floor = Math.Floor(i); //1
+        int i2 = (int)i; //1
+        Console.WriteLine(i);
+        Console.WriteLine(round);
+        Console.WriteLine(ceiling);
+        Console.WriteLine(floor);
+        Console.WriteLine(i2);
+        #endregion
+
+        System.Console.WriteLine("_____________");
+        double amount = 1.11; //количество биткоинов от одного человека
+        int peopleCount = 60; // количество человек  66.6
+        double totalMoney = (double)(amount * peopleCount); // ← исправьте ошибку в этой строке
+        Console.WriteLine(totalMoney);
+
+        char k = '&'; // 75
+        int kInt = (int)k;
+        System.Console.WriteLine(kInt);
+        */
+
+        //System.Console.Write("Input a: ");
+        //int input = Convert.ToInt32(Console.ReadLine());
+        
+        /*
+        System.Console.WriteLine("Are you studying?");
+        string input = Console.ReadLine();
+        //bool vova_is_bad_boy = false;
+        
+        string bad = "You are a bad boy!";
+        string good = "You are a good boy :3";
+
+        if(input == "yes")
+        {
+            Console.WriteLine(good);
+        }
+        else if(input == "no")
+        {
+            Console.WriteLine(bad);
+        }
+        else{
+            Console.WriteLine("хз");
+        }
+        */
+        
+        
+        /*
+        int a = 3;
+        int b = 5; 
+
+        bool t = a != b; //true
+        bool t2 = !t;    //false
+
+        int max = a > b ? a : b;
+
+        int max2;
+        if(a > b)
+        {
+            max2 = a;
+        }
+        else{
+            max2 = b;
+        }
+
+
+        if(b > a)
+        {
+            Console.WriteLine("ehfehfieh");
+        }
+        */
+
+        //Дано двузначное число. Найти: а) число десятков в нем; 
+        //б) число единиц в нем; в) сумму его цифр; г) произведение его цифр.
+        
+
+        /*
+        int num = 1789807;
+        int sum = 0; 
+
+        while(num > 0)
+        {
+            int last = num % 10;
+            num = num / 10;
+            sum = sum + last;
+        }          
+        
+        System.Console.WriteLine(sum);
+        */
+        
+
+
+
+    }
+}
