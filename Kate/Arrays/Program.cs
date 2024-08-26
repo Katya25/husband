adf456248dbb88701fe26cdf6d5fb9f5cc4ff17f
Kate <rita15062425@icloud.com>
2024-08-26 16:00:57 +0100

    enjdiendoedo

---
+++ Kate/Arrays/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //                                0      1         2       3         4           5          6
+        string[] names = new string[] {"Vova", "Katya", "Fedor", "Sasha", "Natasha", "Veronika", "Nastya"};
+        
+        names[6] = "Dasha";
+
+        int _i = 20;
+        TryToChangeInt(_i);
+
+        System.Console.WriteLine("i =  " + _i);
+
+        int val = 6;
+        int[] arr = {7, 6, 5, 3, 2, 6, 8};
+
+        // сколько чисел не равны val - ?
+        // вернуть массив в таком же порядке, но удалив все числа == val
+
+        int count = 0;
+        for(int i = 0; i < arr.Length; i++)
+        {
+            if(arr[i] != val)
+            {
+                count++;
+            }
+        }
+        System.Console.WriteLine("Count = " + count);
+
+        int index = 0;
+        int[] arr2 = new int[count];  //{0, 0, 0, 0, 0}
+        for(int i = 0; i < arr.Length; i++)
+        {
+            if(arr[i] != val)
+            {
+                arr2[index] = arr[i];
+                index++;
+            }
+        }
+
+        System.Console.Write("Array2: ");
+        PrintArray(arr2);
+
+
+
+        //RandomArray(arr);
+        //PrintArray(arr);
+
+        //  1 2 3 4 5      int[] arr = new int[] {1, 2, 3, 4, 5};
+        
+
+        SayHelloTo(names);
+    }
+
+    public static void TryToChangeInt(int i)   
+    {
+        i = 10;
+    }
+
+    public static void RandomArray(int[] arr)
+    {
+        Random rand = new Random();
+        for(int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = rand.Next(1, 11);
+        }
+    }
+
+    public static void PrintArray(int[] arr)
+    {
+        foreach(int i in arr)
+        {
+            System.Console.Write(i + " ");
+        }
+        System.Console.WriteLine();
+    }
+
+    public static void SayHelloTo(string[] names)  //первых 5
+    {
+        System.Console.Write("Hello ");
+        for(int i = 0; i < 5; i++)
+        {
+            System.Console.Write(names[i] + ", ");
+        }
+        System.Console.WriteLine();
+    }
+}
--- Kate/loops/Program.cs
+++ Kate/loops/Program.cs
@@ -64,6 +64,7 @@ class Program
         }
         */
 
+        /*
         for(int i = 1; i <= 3; i++)
         {
             for(int j = 1; j <= 3; j++)
@@ -72,6 +73,67 @@ class Program
             }
             System.Console.WriteLine();
         }
+        */
+
+        /*
+        1. Определить, является ли число а делителем числа b
+        2. Создать программу, которая выводит на экран простые числа в диапазоне от 2 до 1000. - тяжело
+        3. Дано целое число N (> 0). Используя операции деления нацело и взятия остатка от деления, 
+        найти количество и сумму его цифр.
+        4. Найти сумму целых положительных чисел из промежутка от a до b, кратных четырем.
+        5. Дано вещественное число — цена 1 кг конфет. Вывести стоимость 1.2, 1.4, …, 2 кг конфет
+        6. Даны целые положительные числа A и B (A < B). Вывести все целые числа от A до B включительно; 
+        при этом каждое число должно выводиться столько раз, каково его значение (например, число 3 
+        выводится 3 раза).
+        */
+
+        //найти есть ли какая-то цифра в введенном пользователем числе
+        System.Console.Write("Введите число: ");
+        int num = int.Parse(Console.ReadLine());  // -12345
+        System.Console.Write("Введите цифру: ");
+        int digit = int.Parse(Console.ReadLine());  // 6
+
+        //-468734992  2 true   0   false
+
+        bool isDigitFound = false;
+
+        if(num == 0 && digit == 0) isDigitFound = true;
+        else if(num == 0) isDigitFound = false;
+        else{
+            num = Math.Abs(num);     // 12345   1234   123  12   1    0
+            while(num > 0)
+            {
+                int cur = num % 10;
+                if(cur == digit)
+                {
+                    isDigitFound = true;
+                    break;
+                }
+                num /= 10;
+            }
+        }
+
+        if(isDigitFound) System.Console.WriteLine("Was found");
+        else System.Console.WriteLine("Was not");
+
+
+
+        for(int i = 2; i < 10; i++)
+        {
+            int half = i / 2;
+            bool isPrime = true;  //1
+            for(int j = 2; j <= half; j++)
+            {
+                if(i % j == 0)
+                {
+                    isPrime = false;  //0
+                    break;
+                } 
+            }
+            if(isPrime) Console.Write(i + ", ");
+        }
+        System.Console.WriteLine();
+
 
     }
 }
